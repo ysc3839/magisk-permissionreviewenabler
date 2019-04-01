0a27d687ef4055934aa3b45eebeae18ff0d58986
Richard Yu <yurichard3839@gmail.com>
2019-04-02 00:42:02 +0800

    Modify install.sh modname.

--- magisk/install.sh
+++ magisk/install.sh
@@ -123,7 +123,7 @@ REPLACE="
 
 print_modname() {
   ui_print "*******************************"
-  ui_print "     Magisk Module Template    "
+  ui_print "   Permission Review Enabler   "
   ui_print "*******************************"
 }
 
